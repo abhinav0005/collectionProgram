6b822ba4504176aef8522abbb1e37fc9a0f761c4
abhinav raj <abhinavraj423@gmail.com>
2018-03-16 20:49:36 +0530

    first commit

---
+++ HasSet2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionApp
+{
+    class HasSet2
+    {
+        static void Main(string[] args)
+        {
+            var city = new HashSet<string>() { "Bangaluru", "New Delhi", "Patna", "Mumbai","1" };
+            foreach(var name in city)
+            {
+                Console.WriteLine(name);
+            }
+            Console.Read();
+        }
+    }
+}
