e97616a28304960b079bc938e9f674c67c279b84
abhinav raj <abhinavraj423@gmail.com>
2018-03-16 20:46:13 +0530

    first commit

---
+++ HasSet1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionApp
+{
+    class HasSet1
+    {
+        static void Main(string[] args)
+        {
+            var names = new HashSet<string>();
+            names.Add("Raj");
+            names.Add("Rohan");
+            names.Add("Sohan");
+            names.Add("Deepak");
+            names.Add("Raj");
+            foreach(var name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.Read();
+        }
+    }
+}
